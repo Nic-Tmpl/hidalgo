054cbccf9da03d887b62d1f201ff79dcbdc21cf7
Nic-Temple <nictemplepiano@gmail.com>
2022-11-09 16:51:17 -0800

    updates productActions to include detailsProduct action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -13,4 +13,14 @@ const listProducts = () =>  async (dispatch) => {
 
 }
 
-export { listProducts };
+const detailsProduct = (productId) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+        const { data } = await axios.get(`api/products/${productId}`);
+        dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
+    } catch(error) {
+        dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message});
+    }
+}
+
+export { listProducts, detailsProduct };
