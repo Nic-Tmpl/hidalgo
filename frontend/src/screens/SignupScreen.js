329e2148fc45ca9f98f9526e6ae406d01b6e6c92
Nic-Temple <nictemplepiano@gmail.com>
2022-11-14 10:47:51 -0800

    formatting in signup screen

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -10,19 +10,19 @@ function SignupScreen(props) {
         <h1>Sign up</h1>
         <form action="/signup" method="post">
             <section>
-                <label for="username">Username</label>
+                <label for="username">Username: </label>
                 <input id="username" name="username" type="text" autocomplete="username" required />
             </section>
             <section>
-                <label for="new-password">Password</label>
+                <label for="new-password">Password: </label>
                 <input id="new-password" name="password" type="password" autocomplete="new-password" required />
             </section>
             <section>
-                <label for="first_name">First Name</label>
+                <label for="first_name">First Name: </label>
                 <input id="first_name" name="first_name" type="text" autocomplete="first_name" required />
             </section>
             <section>
-                <label for="last_name">Last Name</label>
+                <label for="last_name">Last Name: </label>
                 <input id="last_name" name="last_name" type="text" autocomplete="last_name" required />
             </section>
             <button type="submit">Sign up</button>
