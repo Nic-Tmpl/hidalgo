a0049f84a7c9e1def3bb2d6d3c41bc2b75bb9574
Nic-Temple <nictemplepiano@gmail.com>
2022-11-18 15:39:35 -0800

    landing Screen added

---
+++ frontend/src/screens/LandingScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../App.css';
+
+
+
+function LandingScreen() {
+
+
+    return (
+    <div className="landing">
+        <div className="jumbotron">
+            <h1>HIDALGO</h1>
+        </div>
+        <div className="about">
+            <p>You ever get hungry? We get hungry all the time. You ever look at some guy, wearing some nice shirt or a fine pair
+                of chinos, and you want one for yourself? It would be too weird to ask a strange man about his clothing. We know. But here
+                at Hidalgo, we'll load you up with food and if you spot some shirt you like, we'll rip it right off the guy and give it to
+                you. For a price. No questions asked. That's the Hidalgo guarantee. So, is it <Link to='/products'>shirts</Link> you're 
+                looking for? <Link to="/products">Pants</Link>? Or just <Link to="/products">food</Link>?
+            </p>
+        </div>
+        <div className="productBar">
+            <button onClick={redirectHandler}></button>
+        </div>
+     </div>
+    )}
+
+export default LandingScreen;
