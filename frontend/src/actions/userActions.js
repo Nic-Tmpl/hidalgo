87352932373fc80f08d5b30710cebe50043a0d38
Nic-Temple <nictemplepiano@gmail.com>
2022-11-22 14:07:19 -0800

    changes post to /login/password

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,7 +6,7 @@ const login = (email, password) => async (dispatch) => {
     dispatch({type: USER_SIGNIN_REQUEST, payload: {email, password}});
     try {
         //currently not destructuring data as I am uncertain how much info is needed in state.
-        const data = await axios.post("/login", {email: email, password: password});
+        const data = await axios.post("/login/password", {email: email, password: password});
         dispatch({type: USER_SIGNIN_SUCCESS, payload: data});
     } catch (error) {
        dispatch({ type: USER_SIGNIN_FAIL, payload: error.message }); 
