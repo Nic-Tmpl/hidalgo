d7bc8c860e595394f7b83d7dfe9cc19d05dfc64f
Nic-Temple <nictemplepiano@gmail.com>
2022-11-22 19:06:11 -0800

    creates logout useraction and changes reducer imports

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,29 +1,39 @@
 import axios from 'axios';
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNUP_FAIL, USER_SIGNUP_REQUEST, USER_SIGNUP_SUCCESS } from '../constants/userConstants';
+import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT_FAIL, USER_SIGNOUT_REQUEST, USER_SIGNOUT_SUCCESS, USER_SIGNUP_FAIL, USER_SIGNUP_REQUEST, USER_SIGNUP_SUCCESS } from '../constants/userConstants';
 
 
 const login = (email, password) => async (dispatch) => {
-    dispatch({type: USER_SIGNIN_REQUEST, payload: {email, password}});
+    dispatch({ type: USER_SIGNIN_REQUEST, payload: {email, password} });
     try {
         const { data } = await axios.post("/login/password", { username: email, password: password });
-        dispatch({type: USER_SIGNIN_SUCCESS, payload: data});
+        dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
     } catch (error) {
        dispatch({ type: USER_SIGNIN_FAIL, payload: error.message }); 
     }
 }
 
-const signup = (email, password, firstName, lastName) => async (dispatch) => {
-    dispatch({type: USER_SIGNUP_REQUEST, payload: {email, password, firstName, lastName}});
+const logout = () => async (dispatch) => {
+    dispatch({ type: USER_SIGNOUT_REQUEST });
     try {
-        const { data } = await axios.post("signup", {
+        const { data } = await axios.post("/logout");
+        dispatch({ type: USER_SIGNOUT_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({ type: USER_SIGNOUT_FAIL, payload: error.message });
+    }
+}
+
+const signup = (email, password, firstName, lastName) => async (dispatch) => {
+    dispatch({ type: USER_SIGNUP_REQUEST, payload: {email, password, firstName, lastName} });
+    try {
+        const { data } = await axios.post("/signup", {
             email: email, 
             password: password,
             first_name: firstName,
             last_name: lastName,
          });
-         dispatch({type: USER_SIGNUP_SUCCESS, payload: data});
+         dispatch({type: USER_SIGNUP_SUCCESS, payload: data });
     } catch (error) {
-        dispatch({ type: USER_SIGNUP_FAIL, payload: error.message});
+        dispatch({ type: USER_SIGNUP_FAIL, payload: error.message });
     }
 }
 
