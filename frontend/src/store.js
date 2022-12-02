c9c6ae9f303b1c72cdbafe1b408915395fa8d642
Nic-Temple <nictemplepiano@gmail.com>
2022-12-02 08:46:18 -0800

    adds productsave reducer and sends to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
 import { configureStore  } from '@reduxjs/toolkit';
 import { cartReducer } from './reducers/cartReducers';
-import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer, productSaveReducer } from './reducers/productReducers';
 import { userEditReducer, userLoginReducer, userSignupReducer } from './reducers/userReducers';
 
 const reducer = {
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productSave: productSaveReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userSignup: userSignupReducer,
