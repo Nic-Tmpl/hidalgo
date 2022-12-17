0235c44604e26b118cdaca4bf32fc98cb4709afd
Nic-Temple <nictemplepiano@gmail.com>
2022-12-17 12:38:01 -0800

    changes db to use connection string

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,13 +1,23 @@
 const { Pool } = require('pg');
 const { DB } = require('../config');
+const env = process.env.NODE_ENV || 'development';
 
-const pool = new Pool({
+let connectionString = {
     user: DB.PGUSER,
     host: DB.PGHOST,
-    database: DB.PGDATABASE,
+    database:DB.PGDATABASE,
     password: DB.PGPASSWORD,
     port: DB.PGPORT,
-})
+}
+
+if (env !== 'development') {
+    connectionString = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    };
+};
+
+const pool = new Pool(connectionString);
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
