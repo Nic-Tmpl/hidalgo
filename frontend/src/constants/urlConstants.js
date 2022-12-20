7a1c6a337ba009520f0fe161c47aaaff4b25525a
Nic-Temple <nictemplepiano@gmail.com>
2022-12-20 14:14:50 -0800

    debugging

--- frontend/src/constants/urlConstants.js
+++ frontend/src/constants/urlConstants.js
@@ -1,6 +1,6 @@
 //cross-origin URL for localhost production build
 const env = process.env.NODE_ENV || 'development';
-export const API_URL =  'https://localhost:5000';
+export const API_URL =  'https://hidalgo.herokuapp.com';
 
 if (env !== 'development') {
     API_URL = 'https://hidalgo.herokuapp.com'
