58000fb0cf11f85dc2754e7fb3ffa944ecf08817
Nic-Temple <nictemplepiano@gmail.com>
2022-12-20 14:06:12 -0800

    changes apiurl constants and dependencies

--- frontend/src/constants/urlConstants.js
+++ frontend/src/constants/urlConstants.js
@@ -1,5 +1,2 @@
 //cross-origin URL for localhost production build
-export const LOCAL_API_URL = 'http://localhost:5000';
-
-//URL for deployed heroku production build
-export const API_URL = 'http://hidalgo.heroku.com';
+export const API_URL =  process.env.PORT || 'http://localhost:5000';
