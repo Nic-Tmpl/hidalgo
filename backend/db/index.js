b2bc2a0ebfaf77888f391a109d52db9491e371cd
Nic-Temple <nictemplepiano@gmail.com>
2022-12-20 14:00:12 -0800

    change ssl protocol

--- backend/db/index.js
+++ backend/db/index.js
@@ -13,7 +13,7 @@ let connectionString = {
 if (env !== 'development') {
     connectionString = {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: false
     };
 };
 
