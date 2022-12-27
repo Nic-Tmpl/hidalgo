d1466f466aecb573b60d590e800becbe3db837c7
Nic-Temple <nictemplepiano@gmail.com>
2022-12-27 14:59:04 -0800

    changes url string to include api

--- frontend/src/constants/urlConstants.js
+++ frontend/src/constants/urlConstants.js
@@ -1,9 +1,9 @@
 //cross-origin URL for localhost production build
 const env = process.env.NODE_ENV || 'development';
-export var API_URL = 'https://localhost:5000';
+export var API_URL = 'https://localhost:5000/api';
 
 
-if (env !== 'development') {
+/*if (env !== 'development') {
     API_URL =  'https://hidalgo.herokuapp.com';
-}
+}*/
 
