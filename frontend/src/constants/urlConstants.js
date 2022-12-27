136ed3024fea5fe67d5465b4a508b2f735771220
Nic-Temple <nictemplepiano@gmail.com>
2022-12-27 15:01:56 -0800

    typo fix

--- frontend/src/constants/urlConstants.js
+++ frontend/src/constants/urlConstants.js
@@ -1,6 +1,6 @@
 //cross-origin URL for localhost production build
 const env = process.env.NODE_ENV || 'development';
-export var API_URL = 'https://localhost:5000/api';
+export var API_URL = 'http://localhost:5000/api';
 
 
 /*if (env !== 'development') {
